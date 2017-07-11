f03ecc09c5d5bd226548abd58361ed15146820f6
SeongbumSeo <sBum.Seo@gmail.com>
2017-07-11 17:57:20 +0900

    Front controller

--- index.php
+++ index.php
@@ -6,11 +6,8 @@ if(is_file("views/$action.html")) {
 	if(is_file("controllers/$action.php"))
 		require("controllers/$action.php");
 	require("views/$action.html");
-} else if(strlen($action) == 0) {
+} else {
 	$action = "main";
 	goto loop;
-} else {
-	header("Location: http://".$_SERVER["SERVER_NAME"]);
-	exit();
 }
 ?>
