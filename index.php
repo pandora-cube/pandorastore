4fd4243863c90bdf767b0feeae64c50fbe39e106
SeongbumSeo <sBum.Seo@gmail.com>
2017-07-12 17:48:44 +0900

    Front controller

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $action = $_GET["action"];
 
 loop:
-if(is_file("controllers/$action.html")) {
+if(is_file("controllers/$action.php")) {
 	require("controllers/$action.php");
 } else {
 	$action = "main";
