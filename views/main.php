14b4bbfac13f8e1cc8a699d90fd94a9a6fd53997
SeongbumSeo <sBum.Seo@gmail.com>
2017-07-13 21:07:44 +0900

    Main page stylesheet

--- views/main.php
+++ views/main.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
 	<link rel="stylesheet" href="stylesheets/common.css" />
-	<link rel="stylesheet" href="stylesheets/index.css" />
+	<link rel="stylesheet" href="stylesheets/main.css" />
 	<link rel="stylesheet" href="modules/orbit/orbit.css" />
 	<link rel="stylesheet" href="modules/stickymenu/stickymenu.css" />
 	<link rel="icon" href="favicon.ico" type="image/x-icon" />
