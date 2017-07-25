f9e920c8a1d9a7c5d198074a497f8b76aa2a5828
SeongbumSeo <sBum.Seo@gmail.com>
2017-07-25 13:58:23 +0900

    Modal

---
+++ modules/modal/modal.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	$(".modal-origin").each(function() {
+		var origin = this;
+
+		this.open = function(closebutton=true) {
+			createModal(closebutton);
+			$("body").css("overflow", "hidden");
+		};
+
+		this.close = function() {
+			destroyModal();
+			$("body").css("overflow", "auto");
+		};
+
+		function createModal(closebutton) {
+			var $area = $("<div/>")
+				.attr("id", $(origin).attr("name"))
+				.addClass("modalArea")
+				.on("click", function(e) {
+					if(e.target != this) return;
+					origin.close();
+				})
+				.prependTo("body");
+
+			var $modal = $(origin).clone(true)
+				.removeAttr("name")
+				.removeClass("modal-origin")
+				.addClass("modal")
+				.prependTo($area);
+
+			if(closebutton)
+				$("<a/>")
+					.attr("href", "javascript:;")
+					.addClass("closebutton")
+					.on("click", function(e) {
+						if(e.target != this) return;
+						origin.close();
+					})
+					.html("&#120684;")
+					.prependTo($modal);
+		}
+
+		function destroyModal() {
+			$(".modalArea#"+$(origin).attr("name"))
+				.remove();
+		}
+	});
+});
