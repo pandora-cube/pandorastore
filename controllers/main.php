1150f74cdb0098134fba6b283492cf6a26e6756c
SeongbumSeo <sBum.Seo@gmail.com>
2017-08-08 14:41:07 +0900

    수정

--- controllers/main.php
+++ controllers/main.php
@@ -4,16 +4,14 @@ require("models/categories.php");
 require("models/games.php");
 $dbconfig = parse_ini_file("configs/database.ini");
 
-$mysqli = mysqli_connect($dbconfig["host"], $dbconfig["user"], $dbconfig["password"], $dbconfig["database"]);
+$mysqli = mysqli_connect($dbconfig["host"], $dbconfig["user"], $dbconfig["password"], $dbconfig["database"]); {
+	$orbit = new Orbit($mysqli, $dbconfig["table"]);
+	$orbit = $orbit->load();
 
-$orbit = new Orbit($mysqli, $dbconfig["table"]);
-$orbit = $orbit->load();
-
-$categories = new Categories($mysqli, $dbconfig["table"]);
-$games = new Games($mysqli, $dbconfig["table"], $categories);
-$games = $games->load();
-
-$mysqli->close();
+	$categories = new Categories($mysqli, $dbconfig["table"]);
+	$games = new Games($mysqli, $dbconfig["table"], $categories);
+	$games = $games->load();
+} $mysqli->close();
 
 require("views/main.php");
 ?>
