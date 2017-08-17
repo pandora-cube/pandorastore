5ad6644741190d34ad2f6a627a333e88f55171d6
SeongbumSeo <sBum.Seo@gmail.com>
2017-08-17 23:28:36 +0900

    Modal class화

--- modules/modal/modal.js
+++ modules/modal/modal.js
@@ -1,49 +1,51 @@
 $(document).ready(function() {
-	$(".modal-origin").each(function() {
-		var origin = this;
+	$(".modal-origin").each(Modal);
+});
 
-		this.open = function(closebutton) {
-			closebutton = closebutton || true;
-			createModal(closebutton);
-			$("body").css("overflow", "hidden");
-		};
+function Modal() {
+	var origin = this;
 
-		this.close = function() {
-			destroyModal();
-			$("body").css("overflow", "auto");
-		};
+	this.open = function(closebutton) {
+		closebutton = closebutton || true;
+		createModal(closebutton);
+		$("body").css("overflow", "hidden");
+	};
 
-		function createModal(closebutton) {
-			var $area = $("<div/>")
-				.attr("id", $(origin).attr("name"))
-				.addClass("modalArea")
+	this.close = function() {
+		destroyModal();
+		$("body").css("overflow", "auto");
+	};
+
+	function createModal(closebutton) {
+		var $area = $("<div/>")
+			.attr("id", $(origin).attr("name"))
+			.addClass("modalArea")
+			.on("click", function(e) {
+				if(e.target != this) return;
+				origin.close();
+			})
+			.prependTo("body");
+
+		var $modal = $(origin).clone(true)
+			.removeAttr("name")
+			.removeClass("modal-origin")
+			.addClass("modal")
+			.prependTo($area);
+
+		if(closebutton)
+			$("<a/>")
+				.attr("href", "javascript:;")
+				.addClass("closebutton")
 				.on("click", function(e) {
 					if(e.target != this) return;
 					origin.close();
 				})
-				.prependTo("body");
+				.html("&#120684;")
+				.prependTo($modal);
+	}
 
-			var $modal = $(origin).clone(true)
-				.removeAttr("name")
-				.removeClass("modal-origin")
-				.addClass("modal")
-				.prependTo($area);
-
-			if(closebutton)
-				$("<a/>")
-					.attr("href", "javascript:;")
-					.addClass("closebutton")
-					.on("click", function(e) {
-						if(e.target != this) return;
-						origin.close();
-					})
-					.html("&#120684;")
-					.prependTo($modal);
-		}
-
-		function destroyModal() {
-			$(".modalArea#"+$(origin).attr("name"))
-				.remove();
-		}
-	});
-});
+	function destroyModal() {
+		$(".modalArea#"+$(origin).attr("name"))
+			.remove();
+	}
+}
