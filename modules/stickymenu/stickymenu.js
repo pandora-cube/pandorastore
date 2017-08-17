a0ae7a221769f0fcb396c71db3a2ff0a31573854
SeongbumSeo <sBum.Seo@gmail.com>
2017-08-18 01:43:39 +0900

    Stickymenu

--- modules/stickymenu/stickymenu.js
+++ modules/stickymenu/stickymenu.js
@@ -1,36 +1,17 @@
 $(document).ready(function() {
 	$(window).on("scroll", onScroll);
 
-	$(".menuArea").hover(onMouseEnter, onMouseLeave);
-	$(".menuArea .menu > li").hover(onMouseEnter, onMouseLeave);
-
 	function onScroll() {
 		var scrolltop = $(window).scrollTop();
 		var $menuarea = $(".menuArea[class!=fixed]");
 		var menutop = $menuarea.position().top;
 
 		if(scrolltop > menutop && $(".menuArea.fixed").length == 0) {
-			$menuarea
-				.removeClass("enabled")
-				.find("li, .inner-menu")
-					.removeClass("enabled");
 			$menuarea.clone(true)
 				.appendTo($menuarea.parent())
 				.addClass("fixed");
 		} else if(scrolltop < menutop) {
 			$(".menuArea.fixed").remove();
 		}
-	}
-
-	function onMouseEnter(e) {
-		$(e.currentTarget).addClass("enabled");
-		$(e.currentTarget).children(".inner-menu")
-			.addClass("enabled");
-	}
-
-	function onMouseLeave(e) {
-		$(e.currentTarget).removeClass("enabled");
-		$(e.currentTarget).children(".inner-menu")
-			.removeClass("enabled");
 	}
 });
