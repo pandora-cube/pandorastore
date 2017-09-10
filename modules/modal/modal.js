ead50cd9b48b547f94ca2cc23e9a107483a863af
SeongbumSeo <sBum.Seo@gmail.com>
2017-09-11 08:42:03 +0900

    modal.js lint

--- modules/modal/modal.js
+++ modules/modal/modal.js
@@ -1,67 +1,65 @@
-$(document).ready(function() {
-	$(".modal-origin").each(Modal);
-});
-
 function Modal() {
-	var origin = this;
+    const origin = this;
 
-	this.open = function(closebutton) {
-		closebutton = closebutton || true;
+    function onCloseButtonClicked(e) {
+        if (e.target !== this) return;
+        origin.close();
+    }
 
-		var scrollTop = $(window).scrollTop();
-		var height = $(document).height();
-		$(this).data("scrollTop", scrollTop);
-		$("html, body").addClass("notScroll");
-		$("body")
-			.css("top", -scrollTop)
-			.css("height", height);
+    function createModal(closebutton) {
+        const $area = $("<div/>")
+            .attr("id", $(origin).attr("name"))
+            .addClass("modalArea")
+            .on("click", onCloseButtonClicked)
+            .prependTo("body");
 
-		return createModal(closebutton);
-	};
+        const $modal = $("<div/>")
+            .html($(origin).html())
+            .removeAttr("name")
+            .removeClass("modal-origin")
+            .addClass("modal")
+            .prependTo($area);
 
-	this.close = function() {
-		$("body")
-			.css("top", 0)
-			.css("height", "auto");
-		$("html, body").removeClass("notScroll");
-		$(window).scrollTop($(this).data("scrollTop"));
+        if (closebutton) {
+            $("<button/>")
+                .addClass("closebutton")
+                .on("click", onCloseButtonClicked)
+                .html("&#10006;")
+                .prependTo($modal);
+        }
 
-		destroyModal();
-	};
+        return $area;
+    }
 
-	function createModal(closebutton) {
-		var $area = $("<div/>")
-			.attr("id", $(origin).attr("name"))
-			.addClass("modalArea")
-			.on("click", function(e) {
-				if(e.target != this) return;
-				origin.close();
-			})
-			.prependTo("body");
+    function destroyModal() {
+        $(`.modalArea#${$(origin).attr("name")}`).remove();
+    }
 
-		var $modal = $("<div/>")
-			.html($(origin).html())
-			.removeAttr("name")
-			.removeClass("modal-origin")
-			.addClass("modal")
-			.prependTo($area);
+    this.open = function openModal(closebutton_) {
+        const closebutton = closebutton_ || true;
+        const scrollTop = $(window).scrollTop();
+        const height = $(document).height();
 
-		if(closebutton)
-			$("<a/>")
-				.attr("href", "javascript:;")
-				.addClass("closebutton")
-				.on("click", function(e) {
-					if(e.target != this) return;
-					origin.close();
-				})
-				.html("&#10006;")
-				.prependTo($modal);
+        $(this).data("scrollTop", scrollTop);
+        $("html, body").addClass("notScroll");
+        $("body")
+            .css("top", -scrollTop)
+            .css("height", height);
 
-		return $area;
-	}
+        return createModal(closebutton);
+    };
 
-	function destroyModal() {
-		$(".modalArea#"+$(origin).attr("name"))
-			.remove();
-	}
+    this.close = function closeModal() {
+        $("body")
+            .css("top", 0)
+            .css("height", "auto");
+        $("html, body").removeClass("notScroll");
+        $(window).scrollTop($(this).data("scrollTop"));
+
+        destroyModal();
+    };
 }
+
+$(document).ready(() => {
+    $(".modal-origin").each(Modal);
+});
