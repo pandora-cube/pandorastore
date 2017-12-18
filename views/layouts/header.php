8b83c5b33a91f0e046e7d7315fc211b32a590115
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-18 23:44:07 +0900

    메뉴 UI 복원

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -22,46 +22,38 @@
 <div class="blankArea"></div>
 <!-- 메뉴 -->
 <div class="menuArea">
-    <div class="menu-wrapper">
-        <!-- 드롭다운 메뉴 -->
-        <ul class="menu">
-            <li>
-                <div class="title-wrapper" tabindex="0">
-                    <span class="title">판도라</span>
-                </div>
-                <ul class="inner-menu">
-                    <li><a>준비중인 메뉴입니다.</a></li>
-                </ul>
-            </li>
-            <li>
-                <div class="title-wrapper" tabindex="0">
-                    <span class="title">게임</span>
-                </div>
-                <ul class="inner-menu">
-                    <li><a href="/"><b>전체</b></a></li>
-                    <li><a href="/category/G1">타이쿤</a></li>
-                    <li><a href="/category/G2">어드벤처</a></li>
-                    <li><a href="/category/G4">전략</a></li>
-                    <li><a href="/category/G5">퍼즐</a></li>
-                    <li><a href="/category/G6">보드</a></li>
-                    <li><a href="/category/G7">액션</a></li>
-                    <li><a href="/category/G8">캐주얼</a></li>
-                    <li><a href="/category/G9">아케이드</a></li>
-                    <li><a href="/category/G10">시뮬레이션</a></li>
-                </ul>
-            </li>
-            <li>
-                <div class="title-wrapper">
-                    <a class="title" href="/category/G3">소프트웨어</a>
-                </div>
-            </li>
-        </ul>
-        <!-- 검색UI -->
-        <form class="search" action="/" method="get">
-            <input name="search" type="text" placeholder="검색어를 입력해 주세요" />
-            <input type="submit" value="검색" />
-        </form>
-    </div>
+    <ul class="menu">
+        <li>
+            <div class="title-wrapper" tabindex="0">
+                <span class="title">판도라</span>
+            </div>
+            <ul class="inner-menu">
+                <li><a>준비중인 메뉴입니다.</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="title-wrapper" tabindex="0">
+                <span class="title">게임</span>
+            </div>
+            <ul class="inner-menu">
+                <li><a href="/"><b>전체</b></a></li>
+                <li><a href="/category/G1">타이쿤</a></li>
+                <li><a href="/category/G2">어드벤처</a></li>
+                <li><a href="/category/G4">전략</a></li>
+                <li><a href="/category/G5">퍼즐</a></li>
+                <li><a href="/category/G6">보드</a></li>
+                <li><a href="/category/G7">액션</a></li>
+                <li><a href="/category/G8">캐주얼</a></li>
+                <li><a href="/category/G9">아케이드</a></li>
+                <li><a href="/category/G10">시뮬레이션</a></li>
+            </ul>
+        </li>
+        <li>
+            <div class="title-wrapper">
+                <a class="title" href="/category/G3">소프트웨어</a>
+            </div>
+        </li>
+    </ul>
 </div>
 <!-- 이미지 슬라이드 -->
 <div id="topOrbit" class="orbitArea"></div>
