d7b589e1999ca65e74db3f20505bbe85231ef72a
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-29 20:35:31 +0900

    구글 애널리틱스 등록

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -5,3 +5,11 @@
 <link rel="stylesheet" href="/modules/stickymenu/stickymenu.css" />
 <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 <title><?=$this->title?></title>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111679881-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-111679881-1');
+</script>
