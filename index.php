09760323576a70cd06f36622bb8a54af82f144f7
SeongbumSeo <sBum.Seo@gmail.com>
2017-12-30 19:27:24 +0900

    Logger 추가

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
+require_once("logger.php");
+require_once("models/user.php");
+
 $action = $_GET["action"];
+
+// 설정 불러오기
+$config_db = parse_ini_file("configs/database.ini");
+
+// DB 접속
+$mysqli = mysqli_connect($config_db["host"], $config_db["user"], $config_db["password"], $config_db["database"]);
+if (!$mysqli) { // 실패 시
+    return;
+}
 
 loop:
 if(is_file("controllers/$action.php")) {
     require("controllers/$action.php");
+
+    // 페이징 로그
+    $user = new User($mysqli, $config_db["table"]);
+    $logger = new Logger($mysqli, $config_db["table"], $user);
+    $logger->logPaging();
 } else {
     $action = "main";
     goto loop;
---
+++ logger.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 전체 사이트에서 광범위하게 사용되는 로그 기능
+ * 특정 범위에서만 사용되는 로그는 해당 모듈 내에 별도 구현
+ */
+class Logger {
+    private $mysqli;
+    private $table;
+    private $user_data;
+
+    public function __construct($link, $table, $user_model) {
+        $this->mysqli = $link;
+        $this->table = $table;
+
+        session_start();
+        if (isset($_SESSION["UserID"]) && isset($_SESSION["Password"])) {
+            $this->user_data = $user_model->load($_SESSION["UserID"], $_SESSION["Password"], false);
+        } else {
+            $this->user_data = null;
+        }
+    }
+
+    public function logPaging() {
+        $userIP = $this->mysqli->escape_string($_SERVER["REMOTE_ADDR"]);
+        $userNumber = ($this->user_data !== null) ? $this->user_data["UserNumber"] : "NULL";
+        $url = $this->mysqli->escape_string("{$_SERVER["HTTP_HOST"]}{$_SERVER["REQUEST_URI"]}");
+
+        $sql = "
+            INSERT INTO {$this->table["log_paging"]} (UserIP, UserNumber, URL)
+            VALUES (\"{$userIP}\", {$userNumber}, \"{$url}\")";
+        
+        $this->mysqli->query($sql);
+    }
+}
+?>
