194e8a50be5e64b96db8cfc6817218b3be6f6a1e
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-03 21:38:56 +0900

    회원가입 판도라큐브 회원일 시

--- views/accounts/signup.php
+++ views/accounts/signup.php
@@ -13,6 +13,7 @@
     <div id="contents">
         <form class="signup-form" action="/accounts/createaccount" method="post">
             <h2>판도라스토어 회원 가입</h2>
+
             <label for="LastName">성명</label>
             <span class="multi">
                 <input id="LastName" name="LastName" type="text" placeholder="성" required />
@@ -23,6 +24,10 @@
             <label for="Password">비밀번호</label>
             <input id="Password" name="Password" type="password" placeholder="비밀번호 생성" required />
             <input name="PasswordAgain" type="password" placeholder="비밀번호 확인" required />
+
+            <label for="StudentID"><i>판도라큐브 회원인 경우 학번을 입력해 주십시오.</i></label>
+            <input id="StudentID" name="StudentID" type="text" />
+
             <input type="submit" value="회원 가입" />
         </form>
     </div>
