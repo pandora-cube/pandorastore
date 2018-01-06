1c65f109db9cfc0f8bf72787c3a092622835ec5a
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-06 18:38:23 +0900

    Users 모델 추가

---
+++ models/users.php
@@ -0,0 +1,34 @@
+<?php
+class Users {
+    private $mysqli;
+    private $table;
+    private $data;
+
+    public function __construct($link, $table) {
+        $this->mysqli = $link;
+        $this->table = $table;
+    }
+
+    public function load($conditions) {
+        $sql = "
+            SELECT *
+            FROM {$this->table["users"]}";
+        
+        if (count($conditions) > 0) {
+            $sql .= " WHERE";
+            foreach ($conditions as $condition) {
+                $value = $this->mysqli->escape_string($condition[2]);
+                $sql .= " ({$condition[0]} {$condition[1]} \"{$value}\")";
+            }
+        }
+        
+        $this->data = null;
+        if ($result = $this->mysqli->query($sql)) {
+            if ($result->num_rows > 0)
+                $this->data = $result->fetch_assoc();
+            $result->free();
+        }
+        return $this->data;
+    }
+}
+?>
