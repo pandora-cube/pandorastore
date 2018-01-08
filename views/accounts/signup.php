17954c256fd6c52f10367973b7255a5b914f4efa
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-08 12:19:20 +0900

    회원가입 폼

--- views/accounts/signup.php
+++ views/accounts/signup.php
@@ -11,22 +11,47 @@
     </header>
 
     <div id="contents">
-        <form class="signup-form" action="/accounts/createaccount" method="post">
+        <form class="signup-form" action="/accounts/requestsignup" method="post">
             <h2>판도라스토어 회원 가입</h2>
 
-            <label id="labelLastName" for="LastName">성명 <span class="alert"></span></label>
-            <span class="multi">
-                <input id="LastName" name="LastName" type="text" placeholder="성" required />
-                <input name="FirstName" type="text" placeholder="이름" required />
-            </span>
+            <label id="labelNickname" for="Nickname">닉네임 <span class="alert"></span></label>
+            <input id="Nickname" name="Nickname" type="text" required />
+
             <label id="labelUserID" for="UserID">이메일 <span class="alert"></span></label>
             <input id="UserID" name="UserID" type="email" required />
+
             <label id="labelPassword" for="Password">비밀번호 <span class="alert"></span></label>
             <input id="Password" name="Password" type="password" placeholder="비밀번호 생성" required />
             <input id="PasswordAgain" name="PasswordAgain" type="password" placeholder="비밀번호 확인" required />
 
-            <label for="StudentID"><i>판도라큐브 회원인 경우 학번을 입력해 주십시오.</i></label>
-            <input id="StudentID" name="StudentID" type="text" />
+            <div>
+                <input id="PCubeMember" name="PCubeMember" type="checkbox" />
+                <label for="PCubeMember">판도라큐브 회원입니다.</label>
+            </div>
+
+            <div class="inner-form">
+                <label id="labelLastName" for="LastName">성명 <span class="alert"></span></label>
+                <span class="multi">
+                    <input id="LastName" name="LastName" type="text" placeholder="성" required />
+                    <input id="FirstName" name="FirstName" type="text" placeholder="이름" required />
+                </span>
+
+                <label id="labelUniv" for="Univ">소속 대학 및 학번<span class="alert"></span></label>
+                <span class="multi">
+                    <input id="Univ" name="Univ" type="text" placeholder="대학명 (ex. 판도라대학교)" />
+                    <input id="StudentID" name="StudentID" type="number" placeholder="학번 (ex. 180108)" />
+                </span>
+                
+                <label id="labelPart">파트 <span class="alert"></span></label>
+                <div class="parts">
+                    <input id="ProgrammingPart" name="Part" type="radio" value="Programming" />
+                    <label for="ProgrammingPart">프로그래밍</label>
+                    <input id="DesignPart" name="Part" type="radio" value="Design" />
+                    <label for="DesignPart">디자인</label>
+                    <input id="ArtPart" name="Part" type="radio" value="Art" />
+                    <label for="ArtPart">아트</label>
+                </div>
+            </div>
 
             <input type="submit" value="회원 가입" />
         </form>
