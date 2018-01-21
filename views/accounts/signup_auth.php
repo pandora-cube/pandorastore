e8be8b8e7b12c0cea6b536a4ae4d1d6583696217
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-21 14:59:55 +0900

    회원 가입 인증 메일의 뷰

---
+++ views/accounts/signup_auth.php
@@ -0,0 +1,6 @@
+<p>판도라스토어 회원 가입 인증 코드는 다음과 같습니다.</p>
+<div style="display: inline-block; margin: 10px; padding: 10px; border: 2px solid #aaa;">
+    <?=$this->getAttribute("auth_code")?>
+</div>
+<p>이용해 주셔서 감사합니다.</p>
+<p>서성범 올림.</p>
