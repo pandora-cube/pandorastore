90eda0aee4e0677773329718c697d8211f565fd5
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-23 09:34:46 +0900

    회원 가입 인증 메일의 뷰
    버그 수정

--- views/accounts/signup_auth.php
+++ views/accounts/signup_auth.php
@@ -1,4 +1,4 @@
-<p><?$this->getAttribute("nickname")?>님, 안녕하세요.</p>
+<p><?=$this->getAttribute("nickname")?> 님, 안녕하세요.</p>
 <p>판도라스토어 회원 가입 인증코드는 다음과 같습니다.</p>
 <div style="display: inline-block; margin: 10px; padding: 10px; border: 2px solid #aaa;">
     <?=$this->getAttribute("auth_code")?>
