622f15e47553cf21cf9f1068aed366fdaf9d18a9
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-26 09:20:37 +0900

    회원 가입 제출 전 유효성 검사

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,6 +1,7 @@
 function onAccountCheckResponse(name, json, focus = false) {
     var data = $.parseJSON(json);
     var $label = $("label[for=" + name + "]");
+    var $dest = $("#" + name);
 
     if (data[0] === 0) { // 사용 불가능한 계정인 경우
         $label
@@ -8,12 +9,14 @@ function onAccountCheckResponse(name, json, focus = false) {
             .addClass("warning")
             .find(".alert")
             .text(data[1]);
+        $dest.data("checked", false);
     } else if (data[0] === 1) { // 사용 가능한 계정인 경우
         $label
             .removeClass("warning")
             .addClass("satisfy")
             .find(".alert")
             .text(data[1]);
+        $dest.data("checked", true);
     } else { // 잘못된 값이 반환된 경우
         $label
             .removeClass("satisfy")
@@ -25,10 +28,11 @@ function onAccountCheckResponse(name, json, focus = false) {
                 .find(".alert")
                 .text(data[1]);
         }
+        $dest.removeData("checked");
     }
 
     if (focus === true) {
-        $("#" + name).focus();
+        $dest.focus();
     }
 }
 
@@ -69,9 +73,25 @@ $(document).ready(function onDocumentReady() {
         });
     }
 
+    function onSubmit(event) {
+        $("#signup-form input").each(function checkInput() {
+            if (this.name.substr(-5, 5) === "Check") {
+                return;
+            }
+
+            if ((this.required === true && $(this).data("checked") !== true)
+            || $(this).data("checked") === false) {
+                $(this).focus();
+                event.preventDefault();
+            }
+        });
+    }
+
     $("#Nickname, #UserID, #Password, #PasswordCheck")
         .on("change", checkAccount)
         .on("keyup", checkAccount);
     $("#PCubeMember")
         .on("change", toggleInnerForm);
+    $("#signup-form")
+        .on("submit", onSubmit);
 });
