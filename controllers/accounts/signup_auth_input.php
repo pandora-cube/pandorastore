4f32ab4f0e79387506355c57e6e95db0874fa3c3
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-26 19:51:38 +0900

    인증 3회 이상 실패 시 차단

--- controllers/accounts/signup_auth_input.php
+++ controllers/accounts/signup_auth_input.php
@@ -1,15 +1,25 @@
 <?php
 require_once("template.php");
 
+session_start();
+
 $template = new Template();
-if (isset($_GET["email"])) {
-    $template->setAttribute("email", $_GET["email"]);
-    $template->disableArea("auth-error");
-} else {
-    $template->disableArea("auth-input");
-}
-if (is_null($_GET["reinput"])) {
+if ($_SESSION["signup_auth_try"] > 3) { // 3회 이상 실패 시
     $template->disableArea("auth-reinput");
+    $template->disableArea("auth-input");
+    $template->disableArea("email-error");
+} else {
+    $template->disableArea("auth-ban");
+    if (is_null($_GET["reinput"])) { // 재시도 시
+        $template->setAttribute("count", $_SESSION["signup_auth_try"]);
+        $template->disableArea("auth-reinput");
+    }
+    if (isset($_GET["email"])) { // 이메일 전달 유무
+        $template->setAttribute("email", $_GET["email"]);
+        $template->disableArea("email-error");
+    } else {
+        $template->disableArea("auth-input");
+    }
 }
 $template->disableArea("topOrbit");
 $template->loadView("accounts/signup_auth_input");
--- views/accounts/signup_auth_input.php
+++ views/accounts/signup_auth_input.php
@@ -15,7 +15,7 @@
             <h2>판도라스토어 회원 가입</h2>
 
             <?php if ($this->isEnabledArea("auth-reinput")): ?>
-                <p class="warning">입력하신 인증코드가 일치하지 않습니다.</p>
+                <p class="warning">입력하신 인증코드가 일치하지 않습니다. (<?=$this->getAttribute("count")?> / 3)</p>
             <?php endif; ?>
 
             <?php if ($this->isEnabledArea("auth-input")): ?>
@@ -25,10 +25,14 @@
                 <input type="submit" value="인증" />
             <?php endif; ?>
             
-            <?php if ($this->isEnabledArea("auth-error")): ?>
+            <?php if ($this->isEnabledArea("email-error")): ?>
                 <p>오류가 발생하여 인증 과정에 차질이 생겼습니다.</p>
                 <p>인증코드를 이메일로 받으신 경우 동봉된 링크를 통해 회원 가입을 완료해 주십시오.</p>
                 <p>죄송합니다.&nbsp;&nbsp;:(</p>
+            <?php endif; ?>
+
+            <?php if ($this->isEnabledArea("auth-ban")): ?>
+                <p>인증에 3회 이상 실패하여 더 이상 시도하실 수 없습니다.</p>
             <?php endif; ?>
         </form>
     </div>
