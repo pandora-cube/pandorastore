eb4fad62f437b487fefdf19ee732d62a177a28b2
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-28 18:00:40 +0900

    Main 컨트롤러 개편

--- controllers/main.php
+++ controllers/main.php
@@ -4,23 +4,24 @@ require_once("models/orbit.php");
 require_once("models/categories.php");
 require_once("models/contents.php");
 
-$category = $_GET["category"];
-preg_match("/G([0-9]+)/", $category, $genre);
-preg_match("/P([0-9]+)/", $category, $platform);
-preg_match("/T([0-9]+)/", $category, $tag);
-$genre = (count($genre) < 2) ? null : $genre[1];
-$platform = (count($platform) < 2) ? null : $platform[1];
-$tag = (count($tag) < 2) ? null : $tag[1];
+/* 인자 전처리 */ {
+    $category = $_GET["category"];
+    preg_match("/G([0-9]+)/", $category, $genre);
+    preg_match("/P([0-9]+)/", $category, $platform);
+    preg_match("/T([0-9]+)/", $category, $tag);
+    $genre = (count($genre) < 2) ? null : $genre[1];
+    $platform = (count($platform) < 2) ? null : $platform[1];
+    $tag = (count($tag) < 2) ? null : $tag[1];
 
-$search = $_GET["search"];
+    $search = $_GET["search"];
+}
 
-$config_db = parse_ini_file("configs/database.ini");
-$mysqli = mysqli_connect($config_db["host"], $config_db["user"], $config_db["password"], $config_db["database"]); {
-    $orbit_model = new Orbit($mysqli, $config_db["table"]);
+/* 데이터 로드 */ {
+    $orbit_model = new Orbit();
     $categories_model = new Categories();
     $contents_model = new Contents($genre, $platform, $tag, null, $search);
 
-    $orbit_data = $orbit_model->load();
+    $orbit_data = $orbit_model->getData();
     $category_names = $categories_model->getNames();
     $category_tags = $categories_model->getTags();
     $contents_data = $contents_model->getContents();
@@ -40,13 +41,15 @@ $mysqli = mysqli_connect($config_db["host"], $config_db["user"], $config_db["pas
         $category_name = $tag_name;
     else
         $category_name = null;
-} $mysqli->close();
+}
 
-$template = new Template();
-$template->setAttribute("orbit", $orbit_data);
-$template->setAttribute("contents", $contents_data);
-$template->setAttribute("category_name", $category_name);
-$template->setAttribute("tags", $category_tags);
-$template->setAttribute("search", addslashes($search));
-$template->loadView("main");
+/* 뷰 로드 */ {
+    $template = new Template();
+    $template->setAttribute("orbit", $orbit_data);
+    $template->setAttribute("contents", $contents_data);
+    $template->setAttribute("category_name", $category_name);
+    $template->setAttribute("tags", $category_tags);
+    $template->setAttribute("search", addslashes($search));
+    $template->loadView("main");
+}
 ?>
