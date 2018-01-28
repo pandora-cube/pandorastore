c393245021d6f5d8e4084a63458bd1a1f9bb1f8a
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-28 18:00:32 +0900

    Orbit 모델 개편

--- models/orbit.php
+++ models/orbit.php
@@ -2,20 +2,36 @@
 class Orbit {
     private $mysqli;
     private $table;
+    private $data;
 
-    public function __construct($link, $table) {
-        $this->mysqli = $link;
-        $this->table = $table;
+    public function __construct() {
+        $config_db = parse_ini_file("configs/database.ini");
+
+        $this->mysqli = mysqli_connect($config_db["host"], $config_db["user"], $config_db["password"], $config_db["database"]);
+        $this->table = $config_db["table"];
+
+        if ($this->mysqli)
+            $this->load();
     }
 
     public function load() {
-        $data = array();
-        if($result = $this->mysqli->query("SELECT * FROM {$this->table["orbit"]} WHERE Actived = 1 ORDER BY ID ASC")) {
-            for($i = 0; $i < $result->num_rows; $i++)
-                $data[$i] = $result->fetch_assoc();
+        $sql = "
+            SELECT *
+            FROM {$this->table["orbit"]}
+            WHERE Actived = 1
+            ORDER BY ID ASC";
+
+        $this->data = [];
+        if ($result = $this->mysqli->query($sql)) {
+            for ($i = 0; $i < $result->num_rows; $i++)
+                $this->data[$i] = $result->fetch_assoc();
             $result->free();
         }
-        return $data;
+        return $this->data;
+    }
+
+    public function getData() {
+        return $this->data;
     }
 }
 ?>
