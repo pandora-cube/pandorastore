5be07bcf0a36c4dd07ae7352fae8e521fc8eda25
SeongbumSeo <sBum.Seo@gmail.com>
2018-01-30 21:36:54 +0900

    회원 가입 인증 메일 수정

--- views/accounts/signup_auth.php
+++ views/accounts/signup_auth.php
@@ -1,9 +1,9 @@
 <div style="font-family: 'Noto Sans Korean', 'Malgun Gothic', Dotum, Helvetica, Arial, sans-serif;">
 <div style="width: 578px; margin: 0 auto; padding: 10px; border: 1px solid #aaa; border-bottom: none;">
-    <p style="margin: 1em 0; font-weight: bold; font-size: 1.2em; text-align: center;">판도라스토어에 오신 것을 환영합니다.</p>
+    <p style="margin: 1em 0; font-weight: bold; font-size: 1.3em; text-align: center;">판도라스토어에 오신 것을 환영합니다.</p>
     <p style="margin: .7em 0;"><?=$this->getAttribute("nickname")?> 님, 안녕하세요.</p>
-    <p>이 메일은 회원 가입을 완료하기 위한 계정 인증 메일입니다.</p>
-    <p>계정 인증을 완료하려면 아래의 <u>확인 방법 중 하나</u>를 선택하십시오.</p>
+    <p>이 메일은 회원 가입을 마무리하기 위한 계정 인증 메일입니다.</p>
+    <p>계정 인증을 완료하려면 다음의 <u>확인 방법 중 하나</u>를 선택하십시오.</p>
     <p style="margin: .7em 0 0 0; font-weight: bold;">* 아래의 버튼을 클릭하여 주십시오.</p>
     <p style="margin: 0 0 .7em 0;">(계정 인증을 다시 요청한 경우 이 버튼이 유효하지 않을 수 있습니다.)</p>
     <div style="margin: 10px; text-align: center;">
