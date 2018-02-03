51bec461e51e8cfac66c66169bb21b49c3bd436d
SeongbumSeo <sBum.Seo@gmail.com>
2018-02-03 19:46:29 +0900

    회원 가입 인증 메일 Gmail 대응

--- pages/accounts/signup_auth/view.php
+++ pages/accounts/signup_auth/view.php
@@ -2,8 +2,8 @@
 <div style="width: 578px; margin: 0 auto; padding: 10px; border: 1px solid #aaa; border-bottom: none;">
     <p style="margin: 1em 0; font-weight: bold; font-size: 1.3em; text-align: center;">판도라스토어에 오신 것을 환영합니다.</p>
     <p style="margin: .7em 0;"><?=$this->getAttribute("nickname")?> 님, 안녕하세요.</p>
-    <p>이 메일은 회원 가입을 마무리하기 위한 계정 인증 메일입니다.</p>
-    <p>계정 인증을 완료하려면 다음의 <u>확인 방법 중 하나</u>를 선택하십시오.</p>
+    <p style="margin: 0;">이 메일은 회원 가입을 마무리하기 위한 계정 인증 메일입니다.</p>
+    <p style="margin: 0;">계정 인증을 완료하려면 다음의 <u>확인 방법 중 하나</u>를 선택하십시오.</p>
     <p style="margin: .7em 0 0 0; font-weight: bold;">* 아래의 버튼을 클릭하여 주십시오.</p>
     <p style="margin: 0 0 .7em 0;">(계정 인증을 다시 요청한 경우 이 버튼이 유효하지 않을 수 있습니다.)</p>
     <div style="margin: 10px; text-align: center;">
@@ -13,11 +13,11 @@
     <div style="margin: 10px; padding: 7px 10px; background-color: #aaa; color: white;">
         <?=$this->getAttribute("auth_code")?>
     </div>
-    <p>이용해 주셔서 감사합니다.</p>
-    <p>서성범 올림.</p>
+    <p style="margin: 0;">이용해 주셔서 감사합니다.</p>
+    <p style="margin: 0;">서성범 올림.</p>
 </div>
 <div style="width: 580px; margin: 0 auto; padding: 15px 10px; text-align: center; color: #aaa; background-color: #333;">
     <img src="http://store.p-cube.kr/images/logo_dark.png" alt="판도라스토어 로고" style="width: 200px;" />
-    <p>Copyright ⓒ2017 teamVENT. All rights reserved.</p>
+    <p style="margin: 0;">Copyright ⓒ2017 teamVENT. All rights reserved.</p>
 </div>
 </div>
