441bf0bd08fd6bd4bfafc9b0e76c3af94e9d6b98
SeongbumSeo <sBum.Seo@gmail.com>
2018-02-10 12:11:05 +0900

    인증되지 않은 계정으로 로그인 시

--- pages/accounts/signin_auth/controller.php
+++ pages/accounts/signin_auth/controller.php
@@ -12,6 +12,9 @@ if (is_null($user_data)) { // 로그인 실패 시
     $_SESSION["signin_try"]++;
     header("Location: /accounts/signin");
     return;
+} else if ($user_data["Authenticated"] == 0) { // 인증된 계정이 아닌 경우
+    header("Location: /accounts/signup_auth_input?email={$user_data["EMail"]}");
+    return;
 }
 
 unset($_SESSION["signin_try"]);
