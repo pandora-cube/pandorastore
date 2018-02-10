61fdca069a42adcb384baa87eda63272e9b6aac5
SeongbumSeo <sBum.Seo@gmail.com>
2018-02-10 11:59:25 +0900

    리뷰 입력란

--- pages/main/view.php
+++ pages/main/view.php
@@ -26,9 +26,11 @@
         </template>
         <template name="contents-detail" class="modal-origin">
             <div class="top">
+                <!-- 아이콘 -->
                 <div class="cover">
                     <img />
                 </div>
+                <!-- 콘텐츠 정보 -->
                 <div class="summary">
                     <p class="title"></p>
                     <p>제작: <span class="creator"></span></p>
@@ -36,9 +38,32 @@
                     <p>환경: <span class="platforms"></span></p>
                 </div>
             </div>
-            <div class="download">
-            </div>
+
+            <!-- 다운로드 버튼 영역 -->
+            <div class="download"></div>
+
+            <!-- 이미지 슬라이드 -->
             <div class="orbitArea"></div>
+
+            <!-- 리뷰 -->
+            <div class="reviewArea">
+                <div class="header">
+                    <span class="text">리뷰</span>
+                    <span class="num-reviews"></span>
+                </div>
+                <!-- 리뷰 입력 -->
+                <div class="input">
+                    <textarea></textarea>
+                    <button>입력</button>
+                </div>
+                <!-- 리뷰 목록 -->
+                <div class="reviews">
+                    <div class="review">
+                        <p class="writer"></p>
+                        <p class="result"></p>
+                    </div>
+                </div>
+            </div>
         </template>
     </div>
 
