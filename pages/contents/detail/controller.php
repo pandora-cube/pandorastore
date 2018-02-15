c5b7638b21a944e3a369ca903e61d39dba0bb1e4
SeongbumSeo <sBum.Seo@gmail.com>
2018-02-15 20:53:37 +0900

    디버그 메시지 삭제

--- pages/contents/detail/controller.php
+++ pages/contents/detail/controller.php
@@ -3,7 +3,6 @@ require_once("libraries/functions/template.php");
 
 $user = new User();
 $user_data = $user->getData();
-print($user_data["PCubeMember"] != 1);
 
 $template = new Template();
 if ($user_data["PCubeMember"] != 1)
