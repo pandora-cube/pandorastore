1890d49e5762ba7877584fdcb92ce434fbdc9da3
SeongbumSeo <sBum.Seo@gmail.com>
2018-03-10 16:35:52 +0900

    헤더 슬라이드에 링크 적용

--- libraries/scripts/toporbit.js
+++ libraries/scripts/toporbit.js
@@ -1,16 +1,26 @@
 function loadTopOrbitData(data) {
     var $slideWrapper = $("header .slideArea .slideWrapper");
+    var $imageWrapper;
     var i;
 
     for (i = 0; i < data.length; i++) {
-        $("<img>")
-            .appendTo($("<div>")
-                .appendTo($slideWrapper)
-                .addClass("image-wrapper"))
-            .addClass("align-" + data[i].Position)
-            .attr("src", data[i].Image)
-            .attr("title", data[i].Summary)
-            .attr("alt", data[i].Description);
+        if (data[i].URL.length > 0) {
+            $imageWrapper = $("<a>")
+                .addClass("image-wrapper")
+                .attr("href", data[i].URL)
+                .attr("target", "_blank");
+        } else {
+            $imageWrapper = $("<div>")
+                .addClass("image-wrapper");
+        }
+
+        $slideWrapper.append(
+            $imageWrapper.append(
+                $("<img>")
+                    .addClass("align-" + data[i].Position)
+                    .attr("src", data[i].Image)
+                    .attr("title", data[i].Summary)
+                    .attr("alt", data[i].Description))); // ALT 적용되지 않고 있음
     }
 
     $slideWrapper.bxSlider({
