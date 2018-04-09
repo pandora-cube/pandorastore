fd1dc673ca6c960dc59a7f1d53ebbb757c8d2b3f
SeongbumSeo <sBum.Seo@gmail.com>
2018-04-09 15:56:29 +0900

    메타데이터 제공

--- layouts/head/view.php
+++ layouts/head/view.php
@@ -1,5 +1,13 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1" />
+<meta name="description" content="판도라큐브 콘텐츠 스토어" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="판도라스토어" />
+<meta property="og:description" content="판도라큐브 콘텐츠 스토어" />
+<meta property="og:url" content="http://p-store.kr" />
+<meta property="og:image" content="http://p-store.kr/favicon.png" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:alt" content="판도라스토어 로고" />
 
 <title><?=$this->title?></title>
 
