3083d0a8a6e096973def6739e43ac5154a1dde9d
SeongbumSeo <sBum.Seo@gmail.com>
2018-04-12 22:50:56 +0900

    normalize.css 적용

--- layouts/head/view.php
+++ layouts/head/view.php
@@ -15,6 +15,7 @@
 <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="/fonts/NotoSansKR/NotoSansKR-Hestia.css" />
 <link rel="stylesheet" href="/fonts/MaterialIcons/MaterialIcons.css" />
+<link rel="stylesheet" href="/libraries/stylesheets/normalize.css" />
 <link rel="stylesheet" href="/pages/stylesheet.css" />
 
 <script src="/libraries/jquery.js"></script>
