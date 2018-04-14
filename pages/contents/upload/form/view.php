7a45e4410d00605a05d54491a137188833a04675
SeongbumSeo <sBum.Seo@gmail.com>
2018-04-15 04:14:49 +0900

    콘텐츠 등록 폼 - placeholder

--- pages/contents/upload/form/view.php
+++ pages/contents/upload/form/view.php
@@ -16,19 +16,19 @@
             <h2>콘텐츠 등록</h2>
 
             <label for="Title">제목</label>
-            <input id="Title" name="Title" type="text" required />
+            <input id="Title" name="Title" type="text" placeholder="콘텐츠의 제목" required />
 
             <label for="Creator">제작자</label>
             <input id="Creator" name="Creator" type="text" placeholder="팀명(팀원)" required />
 
             <label for="Genre">장르</label>
-            <input id="Genre" name="Genre" type="text" required />
+            <input id="Genre" name="Genre" type="text" placeholder="콘텐츠의 장르" required />
 
             <label for="Platform">환경</label>
-            <input id="Platform" name="Platform" type="text" required />
+            <input id="Platform" name="Platform" type="text" placeholder="콘텐츠를 이용 가능한 플랫폼" required />
 
             <label for="Description">소개</label>
-            <textarea id="Description" name="Description" required></textarea>
+            <textarea id="Description" name="Description" placeholder="콘텐츠 열람 시 노출될 소개문" required></textarea>
 
             <label>파일</label>
             <button class="add-file">
@@ -38,19 +38,23 @@
             <ul class="files">
                 <template>
                     <input class="platform-input" name="platform" type="text" placeholder="환경" />
+
                     <div class="url">
                         <input class="url-input" name="url" type="text" placeholder="URL 입력" />
                         <button class="url-apply">
                             <div>확인</div>
                         </button>
                     </div>
+
                     <span class="or">혹은</span>
+
                     <input name="MAX_FILE_SIZE" type="hidden" value="536870912" /> <!-- 최대용량 512MB -->
                     <input class="select-file-input" name="file" type="file" />
                     <button class="select-file-button">
                         <i class="material-icons">&#xE2C6;</i>
                         <span>파일 선택</span>
                     </button> 
+
                     <div class="delete-wrapper">
                         <button class="delete">
                             <span class="blind">삭제</span>
