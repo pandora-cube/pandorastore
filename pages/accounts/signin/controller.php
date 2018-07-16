3845c4f8b4d5be0ce631ef845ffc413ac3e841c4
SeongbumSeo <sBum.Seo@gmail.com>
2018-07-16 21:26:47 +0900

    오타 수정

--- pages/accounts/signin/controller.php
+++ pages/accounts/signin/controller.php
@@ -1,7 +1,7 @@
 <?php
 require_once("libraries/functions/template.php");
 
-$template = new Template("판도라스토어 회원 가입");
+$template = new Template("판도라스토어 로그인");
 $template->disableArea("topOrbit");
 $template->loadView("accounts/signin");
 ?>
