de29dc4e917c7f1695f31f22983e665b63b6c4fd
Seongbuming <sBum.Seo@gmail.com>
2019-05-26 16:10:36 +0900

    콘텐츠 등록 폼 - 팀원 입력 시 키워드에 해당하는 전체 회원 조회

--- pages/contents/upload/get_creator/controller.php
+++ pages/contents/upload/get_creator/controller.php
@@ -11,9 +11,13 @@ $conditions = [
 
 $users_model = new Users();
 $users_data = $users_model->load($conditions, "CreatedTime DESC");
-$creator_data = [
-    UserNumber => $users_data[0]["UserNumber"],
-    Name => $users_data[0]["Name"],
-];
+$creator_data = array();
+foreach ($users_data as $datum) {
+    $map = [
+        UserNumber => $datum["UserNumber"],
+        Name => $datum["Name"],
+    ];
+    array_push($creator_data, $map);
+}
 print(json_encode($creator_data));
 ?>
